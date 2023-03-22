c78fcfb033f5958d7a3ca306727a5d59c8a9a7f7
kcl20 <kcl20@outlook.com>
2023-03-22 06:15:47 -0400

    Removed extra top level folder.

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h1>Download my <a href='/full-stack-developer-portfolio/public/Resume - Kenny Li - Grantbook.docx.pdf'>Resume</a></h1>
+      <p>
+      </p>
+    </div>
+  );
+}
