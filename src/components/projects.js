1fd5d65d556eb598ac63117a56f12cd89955b1fd
kcl20 <kcl20@outlook.com>
2023-03-26 10:30:39 +0500

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,33 @@
 const projects = [
     {
-      title: "Project 1",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      url: "https://example.com/project-1",
+      title: "Cinema Travel Guide",
+      description: "Search for movies related to a destination and also get the weather results.",
+      url: "https://github.com/kcl20/Cinema-Travel-Guide",
     },
     {
-      title: "Project 2",
-      description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      url: "https://example.com/project-2",
+      title: "MongoDB Social Media App",
+      description: "Share thoughts and comments.",
+      url: "https://github.com/kcl20/MongoDB-Social-Media-App",
+    },
+    {
+      title: "E-Commerce Backend",
+      description: "An E-commerce Back End using Object-Relational Mapping.",
+      url: "https://github.com/kcl20/Module-13-Challenge---E-commerce-Back-End" ,
+    },
+    {
+      title: "Employee Tracker",
+      description: "App to track employees and managers.",
+      url: "https://github.com/kcl20/Module-12-Challenge-Employee-Tracker" ,
+    },
+    {
+      title: "Note Taker",
+      description: "Save your notes and thoughts.",
+      url: "https://github.com/kcl20/Module-11-Note-Taker" ,
+    },
+    {
+      title: "Plant.io",
+      description: "A plant care app.",
+      url: "https://github.com/kcl20/Plant.io" ,
     },
     // add more projects here...
   ];
